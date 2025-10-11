537aa0c2a83d511723c7b40dfcba3ee2badf4686
Bhavya-Saravanan <bhsa3618@colorado.edu>
2025-10-11 17:32:01 -0600

    A7 part1:Implemented circular buffer logic

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -16,6 +16,7 @@
 
 #include "aesd-circular-buffer.h"
 
+const uint8_t MAX = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -26,13 +27,47 @@
  * @return the struct aesd_buffer_entry structure representing the position described by char_offset, or
  * NULL if this position is not available in the buffer (not enough data is written).
  */
+
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    uint8_t entries_in_use; 
+    uint8_t idx = buffer->out_offs;
+
+  
+    if (!buffer->full && (buffer->in_offs == buffer->out_offs))
+        return NULL; //as the buffer is empty and nothing to check with the offset
+
+
+    // To find out the entries currently in the buffer
+
+    if (buffer->full) {
+        entries_in_use = MAX;  
+    } else {
+       entries_in_use = ((buffer->in_offs + MAX - buffer->out_offs) % MAX);
+    }
+
+    size_t total_seen = 0; 
+    
+    for (uint8_t i = 0; i < entries_in_use; i++) {
+
+        // Getting the current entry pointer
+        struct aesd_buffer_entry *entry = &buffer->entry[idx];
+
+        //char_offset falls inside this entry
+        if (char_offset < total_seen + entry->size) {
+            *entry_offset_byte_rtn = char_offset - total_seen; // To calculate position within this entry
+            return entry;
+        }
+
+        
+        total_seen += entry->size;
+        idx  = ((idx  + 1) % MAX);
+    }
+
+   
     return NULL;
+   
 }
 
 /**
@@ -44,9 +79,17 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    buffer->entry[buffer->in_offs] = *add_entry; 
+
+    // Moving the out_offs forward so oldest entry always points to the true oldest.
+    if (buffer->full) {
+        buffer->out_offs = ((buffer->out_offs + 1) % MAX);
+    }
+
+    buffer->in_offs = ((buffer->in_offs + 1) % MAX);
+    
+    if (buffer->in_offs == buffer->out_offs)
+        buffer->full = true;
 }
 
 /**
